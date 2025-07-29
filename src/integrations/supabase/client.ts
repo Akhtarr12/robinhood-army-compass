bda283178e22a9346513907963fd52800a48f9d9
Akhtarr12 <akhtarrr012@gmail.com>
2025-07-30 04:49:28 +0530

    fix issue

--- src/integrations/supabase/client.ts
+++ src/integrations/supabase/client.ts
@@ -1,20 +1,14 @@
-
 import { createClient } from '@supabase/supabase-js';
 import type { Database } from './types';
 
 const SUPABASE_URL = process.env.NEXT_PUBLIC_SUPABASE_URL!;
 const SUPABASE_PUBLISHABLE_KEY = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
 
-let supabase: any;
-
-if (typeof window !== "undefined") {
-  supabase = createClient<Database>(SUPABASE_URL, SUPABASE_PUBLISHABLE_KEY, {
-    auth: {
-      storage: window.localStorage,
-      persistSession: true,
-      autoRefreshToken: true,
-    }
-  });
-}
-
+export const supabase = createClient<Database>(SUPABASE_URL, SUPABASE_PUBLISHABLE_KEY, {
+  auth: {
+    storage: localStorage,
+    persistSession: true,
+    autoRefreshToken: true,
+  }
+});
 export { supabase };
